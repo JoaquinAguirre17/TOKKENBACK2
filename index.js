6d447d54bd1451fb02b387b8bc3d95cf73b702be
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2025-03-23 10:08:00 -0300

    hola

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const productRoutes = require('./routes/productRoutes');
+const productRoutes = require('./routes/shopifyRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
