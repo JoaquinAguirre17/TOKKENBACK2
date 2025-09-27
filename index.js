ac0e90bd6621792a7828929a75c7ecabed9df559
Joaquin Aguirre <joaquinaguirre17@gmail.com>
2025-09-27 13:16:42 -0300

    incorporacion de mongo22

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ const { MONGO_URI, PORT = 10000 } = process.env;
     });
   } catch (err) {
     console.error("❌ Error de conexión:", err?.message || err);
-    //l Opcional: process.exit(1); en Render se rgeintenta san
+    //l Opcional: process.exit(1); en Render gse rgeintenta san
   }
 })();
